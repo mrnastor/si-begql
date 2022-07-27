f600afad71e508d547140cdd1f35503bf87bc3b0
dio <gjosol@deloitte.com>
2022-07-27 16:18:19 +0800

    cors fix

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.use((req, res, next) => {
     next()
 })
 app.use(cors())
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(
     "/graphql",
     graphqlHTTP({
